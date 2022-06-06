2c3275cedc7f4f11696ef7db0b353ac5177993db
AlfaizGhazi <captainalfaiz@gmail.com>
2022-06-06 15:39:35 +0530

    fetch movies done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,28 @@
 import * as React from "react";
 import "./Home.scss";
 
+import movieApi from "../../Common/API/movieApi";
+import { API_KEY } from "../../Common/API/movieApiKey";
+import { MovieListing } from "../../Components";
+
 const Home = () => {
+  React.useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${API_KEY}&s=${movieText}&type=movie`)
+        .catch((error) => {
+          console.log("Error: ", error);
+        });
+      console.log("Response: ", response);
+    };
+    fetchMovies();
+  }, []);
+
   return (
     <div className="home">
-      <h1>Home</h1>
+      <div className="banner-img"></div>
+      <MovieListing />
     </div>
   );
 };
