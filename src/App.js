2c442b8537022f222e84de0c4910c1789d082dc8
AlfaizGhazi <captainalfaiz@gmail.com>
2022-06-06 12:53:48 +0530

    implemeted routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import * as React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.scss";
+
+import { Header, Home, MovieDetail, PageNotFound, Footer } from "./Components";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>React Redux Toolkit Movie App</h1>
+    <div className="app">
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path={"/"} component={Home} />
+          <Route exact path={"/home"} component={Home} />
+          <Route exact path={"/movie/:imdbID"} component={MovieDetail} />
+          <Route component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 };
---
+++ src/Common/API/movieApiKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = "3f8d652e";
