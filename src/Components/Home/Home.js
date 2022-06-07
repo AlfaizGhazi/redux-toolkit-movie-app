39440984bb783a1b664e02865275fc25832e9f04
AlfaizGhazi <captainalfaiz@gmail.com>
2022-06-07 15:32:26 +0530

    Add fetch movies functionality and render to DOM

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,15 @@
 import * as React from "react";
+import { useDispatch } from "react-redux";
 import "./Home.scss";
 
 import movieApi from "../../Common/API/movieApi";
 import { API_KEY } from "../../Common/API/movieApiKey";
 import { MovieListing } from "../../Components";
+import { addMovies } from "../../Features/Movies/movieSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
   React.useEffect(() => {
     const movieText = "Harry";
     const fetchMovies = async () => {
@@ -14,10 +18,11 @@ const Home = () => {
         .catch((error) => {
           console.log("Error: ", error);
         });
-      console.log("Response: ", response);
+      // console.log("Response: ", response);
+      dispatch(addMovies(response.data));
     };
     fetchMovies();
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="home">
--- src/Components/MovieListing/MovieListing.js
+++ src/Components/MovieListing/MovieListing.js
@@ -1,10 +1,32 @@
 import * as React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../../Features/Movies/movieSlice";
+import { MovieCard } from "../MovieCard";
 import "./MovieListing.scss";
 
 const MovieListing = () => {
+  const movies = useSelector(getAllMovies);
+
+  const renderMovies = React.useMemo(() => {
+    if (movies?.Response === "True") {
+      return movies.Search.map((movie, index) => {
+        return <MovieCard key={index} data={movie} />;
+      });
+    } else {
+      return (
+        <div className="movies-error">
+          <h3>{movies.Error}</h3>
+        </div>
+      );
+    }
+  }, [movies]);
+
   return (
-    <div className="movie--listing">
-      <h1>MovieListing</h1>
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
     </div>
   );
 };
