21faa8ee7ada32747ee2edfac024241f400e7400
mohd-alfaiz-khan <captainalfaiz@gmail.com>
2022-06-08 18:43:37 +0530

    Add page not found route

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 import "./Header.scss";
 
-import { user, pnf } from "../../Images";
+import { user } from "../../Images";
 import {
   fetchAsyncMovies,
   fetchAsyncShows,
--- src/Components/PageNotFound/PageNotFound.js
+++ src/Components/PageNotFound/PageNotFound.js
@@ -1,10 +1,13 @@
 import * as React from "react";
 import "./PageNotFound.scss";
 
+import { pnf } from "../../Images";
+
 const PageNotFound = () => {
   return (
-    <div className="page--not--found">
-      <h1>PageNotFound</h1>
+    <div className="page-not-found">
+      {/* <h1>PageNotFound</h1> */}
+      <img src={pnf} alt={"page-not-found"} />
     </div>
   );
 };
